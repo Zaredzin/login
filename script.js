183c385a7d33b56fd095365c8ada5d5af0dbe527
Zaredzin <JammedMist76@hotmail.com>
2024-06-14 17:03:10 -0600

    tip

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ document.getElementById('login-form').addEventListener('submit', function(event)
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
   
-    // Aquí puedes agregar tu lógica de autenticación
+    // Aqui estan las credenciales
     if (username === 'Persona3' && password === 'Reload') {
-      // Redirigir a otra página
+      // Entramos a la siguiente página
       window.location.href = 'pagina2.html';
     } else {
       document.getElementById('error-message').textContent = 'Usuario o contraseña incorrectos';
